b0bc15605673ba1f64f1f09126ec49cd06b92dc2
jaminvanderberg <jaminvanderberg@yahoo.com>
2017-12-31 19:37:01 -0600

    Items (in progress)

---
+++ src/main/java/jaminv/advancedmachines/init/ItemInit.java
@@ -0,0 +1,14 @@
+package jaminv.advancedmachines.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jaminv.advancedmachines.objects.items.ItemBase;
+import net.minecraft.item.Item;
+
+public class ItemInit {
+
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item TITANIUM_INGOT = new ItemBase();
+}
---
+++ src/main/java/jaminv/advancedmachines/objects/items/ItemBase.java
@@ -0,0 +1,7 @@
+package jaminv.advancedmachines.objects.items;
+
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item {
+
+}
