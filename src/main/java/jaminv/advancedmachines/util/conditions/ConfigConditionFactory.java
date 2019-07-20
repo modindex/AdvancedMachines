76a0982419efa10082d0caa3adc6292c4af44555
jaminvanderberg <jaminvanderberg@yahoo.com>
2019-07-20 18:13:59 -0500

    Machine image update

---
+++ src/main/java/jaminv/advancedmachines/util/conditions/ConfigConditionFactory.java
@@ -0,0 +1,31 @@
+package jaminv.advancedmachines.util.conditions;
+
+import java.util.function.BooleanSupplier;
+
+import com.google.gson.JsonObject;
+
+import jaminv.advancedmachines.util.ModConfig;
+import jaminv.advancedmachines.util.Reference;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class ConfigConditionFactory implements IConditionFactory {
+
+	@Override
+	public BooleanSupplier parse(JsonContext context, JsonObject json) {
+		String type = JsonUtils.getString(json, "type");
+		
+		if (type.equals(Reference.MODID + ":config")) {
+			String value = JsonUtils.getString(json, "value");
+			switch (value) {
+			case "gearIron":
+				return () -> ModConfig.crafting.craftIronGear;
+			default:
+				break;
+			}
+		}
+		return () -> false;
+	}
+
+}
